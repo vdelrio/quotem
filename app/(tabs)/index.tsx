193c4750636275f9f65cbd7bcefad5c0221ac5cb
vdelrio <victor.manuel.toto@gmail.com>
2025-03-31 16:29:02 -0300

    Added recommended method to load fonts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,34 +1,13 @@
-import { useEffect } from "react";
 import { FlatList, StyleSheet } from "react-native";
 import { useRouter } from "expo-router";
-import * as SplashScreen from "expo-splash-screen";
-import {
-  PTSansNarrow_400Regular,
-  useFonts,
-} from "@expo-google-fonts/pt-sans-narrow";
 import { theme } from "@/theme";
 import { useQuoteStore } from "@/store/quoteStore";
 import { QuoteCard } from "@/components/QuoteCard";
 import { QButton } from "@/components/QButton";
 
-SplashScreen.preventAutoHideAsync();
-
 export default function App() {
-  const [fontsLoaded, fontsError] = useFonts({
-    PTSansNarrow_400Regular,
-  });
   const router = useRouter();
   const quotes = useQuoteStore((state) => state.quotes);
-
-  useEffect(() => {
-    if (fontsLoaded || fontsError) {
-      SplashScreen.hideAsync();
-    }
-  }, [fontsLoaded, fontsError]);
-
-  if (!fontsLoaded && !fontsError) {
-    return null;
-  }
 
   return (
     <FlatList
