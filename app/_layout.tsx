d7fcffbd5ad396b13c321c2ae3bfd69bbb5efc26
vdelrio <victor.manuel.toto@gmail.com>
2025-04-03 12:07:32 -0300

    Added splash screen (2)

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,19 +1,23 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 
 export default function Layout() {
   return (
-    <Stack>
-      <Stack.Screen
-        name="(tabs)"
-        options={{ headerShown: false, animation: "fade" }}
-      />
-      <Stack.Screen
-        name="new-quote"
-        options={{
-          presentation: "modal",
-          title: "Nueva cita",
-        }}
-      />
-    </Stack>
+    <>
+      <Stack>
+        <Stack.Screen
+          name="(tabs)"
+          options={{ headerShown: false, animation: "fade" }}
+        />
+        <Stack.Screen
+          name="new-quote"
+          options={{
+            presentation: "modal",
+            title: "Nueva cita",
+          }}
+        />
+      </Stack>
+      <StatusBar style="auto" />
+    </>
   );
 }
